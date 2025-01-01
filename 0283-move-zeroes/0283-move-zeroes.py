2c313d035d94c975834f2d6d341f3a241de56c55
José Rafael Silva Hermoso <56702181+joserafaelSH@users.noreply.github.com>
2025-01-01 03:29:09 -0300

    Time: 7 ms (47.14%), Space: 18.7 MB (20.38%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,27 +3,31 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        p0 = 0
-        p1 = 0 
+        # p0 = 0
+        # p1 = 0 
 
-        if len(nums) == 1: return 
+        # if len(nums) == 1: return 
         
-        while p0 < len(nums) and p1 < len(nums):
-            while p0 < len(nums) and nums[p0] != 0:
+        # while p0 < len(nums) and p1 < len(nums):
+        #     while p0 < len(nums) and nums[p0] != 0:
+        #         p0 += 1
+            
+        #     if p0 == len(nums):
+        #         return 
+            
+        #     while p1 < len(nums) and nums[p1] == 0:
+        #         p1 += 1
+            
+        #     if p1 == len(nums):
+        #         return 
+            
+        #     if p0 < p1:
+        #         nums[p0], nums[p1] =  nums[p1], nums[p0]
+        #     else:
+        #         p1 = p0 + 1
+
+        p0 = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i], nums[p0] =  nums[p0], nums[i]
                 p0 += 1
-            
-            if p0 == len(nums):
-                return 
-            
-            while p1 < len(nums) and nums[p1] == 0:
-                p1 += 1
-            
-            if p1 == len(nums):
-                return 
-            
-            if p0 < p1:
-                nums[p0], nums[p1] =  nums[p1], nums[p0]
-            else:
-                p1 = p0 + 1
-
-
